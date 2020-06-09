c8711d69fb60bc2c63f7e0423a7eae32ae855d22
matbreak <matbreak@jupimail.com>
2020-06-09 23:42:24 +0200

    feat: Setup mail with Syno mail server

--- src/main/java/ch/matfly/suivirecherches/config/ApplicationProperties.java
+++ src/main/java/ch/matfly/suivirecherches/config/ApplicationProperties.java
@@ -1,5 +1,6 @@
 package ch.matfly.suivirecherches.config;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -9,5 +10,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+@EnableEncryptableProperties
 public class ApplicationProperties {
 }
